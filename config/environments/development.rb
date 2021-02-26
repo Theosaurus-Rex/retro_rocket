859411e67d4b0a3d2d4783c22ee3bd0af94738f7
Theosaurus-Rex <theosaurus13@gmail.com>
2021-02-26 14:48:56 +1100

    integrate image upload with cloudinary

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,4 +62,7 @@ Rails.application.configure do
 
   # Devise config
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # Use Cloudinary
+  config.active_storage.service = :cloudinary
 end
